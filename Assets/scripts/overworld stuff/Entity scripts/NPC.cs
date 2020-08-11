c7eda5278b681325dbd510fa6631b458e7be5653
Graves <theredhead242@gmail.com>
2020-08-11 03:06:46 +0000

    yarn camera control prototype

--- Assets/scripts/overworld stuff/Entity scripts/NPC.cs
+++ Assets/scripts/overworld stuff/Entity scripts/NPC.cs
@@ -2,9 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Yarn.Unity;
+using Cinemachine;
 
 public class NPC : MonoBehaviour, IInteractable
 {
+
+    //the npc also needs to load all the custom cameras for its dialogue if it has any
+    //these are then loaded into a dictionary 
+    //this dictionary of camera's is then
+
+    Dictionary<string, CinemachineVirtualCamera> cameraPositions;
+
+
     public Dialogue dialogue;
     public string talkToNode = "";
     public YarnProgram scriptToLoad;
@@ -12,6 +21,8 @@ public class NPC : MonoBehaviour, IInteractable
     {
         //TODO: reimplement with setence object array
         //UIManager.current.NPCStartTalk(dialogue.sentences, dialogue);
+
+        DialogCameraController.current.setCameraObjects(cameraPositions);
 
         FindObjectOfType<DialogueRunner>().StartDialogue(talkToNode);
     }
@@ -21,10 +32,19 @@ public class NPC : MonoBehaviour, IInteractable
         //lol
         dialogue.initDialogue();
 
+
         if (scriptToLoad != null)
         {
             DialogueRunner dialogueRunner = FindObjectOfType<Yarn.Unity.DialogueRunner>();
             dialogueRunner.Add(scriptToLoad);
+        }
+
+        cameraPositions = new Dictionary<string, CinemachineVirtualCamera>();
+        //go through all the camera positions and add them to the dictionary
+        Transform cameraObjs = transform.GetChild(0);
+        for (int i = 0; i < cameraObjs.childCount; i++)
+        {
+            cameraPositions.Add(cameraObjs.GetChild(i).name, cameraObjs.GetChild(i).GetComponent<CinemachineVirtualCamera>());
         }
     }
 }

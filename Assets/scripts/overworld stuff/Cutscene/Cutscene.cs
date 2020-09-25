f5399e72735dffef17ff085ed64ff6a886402940
Graves <theredhead242@gmail.com>
2020-09-25 06:32:09 +0000

    basic cutscene controller
    Former-commit-id: 46e18b7e5d09a5089d7177e291a0c997c50d4baf

--- Assets/scripts/overworld stuff/Cutscene/Cutscene.cs
+++ Assets/scripts/overworld stuff/Cutscene/Cutscene.cs
@@ -1,14 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using Cinemachine;
 
 //each cutscene needs a array of gameobjects in the cutscene to be manipulated by yarn
 //each gameobject needs an array of waypoints 
+
+//CUTSCENES CAN ALSO be used in dialogue to move entities around but are not required. 
+
+//so without dialogue a cutscene is triggered 
+//after it is triggered we then need to know the order in which actions occur (entities moving/camera positions)
+//ideally this should be able to be done via editor 
+
+//first prototype : open a door and show the camera there, then show the player moving towards the door
+//so the first thing that happens here is a camera movement and then a wait 
+//then the player moves then a wait 
+
 public class Cutscene : MonoBehaviour
 {
+
+    public UnityEvent cutsceneActions;
+
     public CutsceneEntity[] entities;
+    public CinemachineVirtualCamera[] cameras;
 
-
+    //also going to maybe need camera positions for some cutscenes
     //need a function that goes through entities list and finds an entity by name
 
     public CutsceneEntity GetEntityByName(string entityName)
@@ -25,7 +42,16 @@ public class Cutscene : MonoBehaviour
         return null;
     }
 
+    public void DisableCameras()
+    {
+        foreach (CinemachineVirtualCamera camera in cameras)
+        {
+            camera.Priority = -420;
+        }
+    }
+
     //this function takes in an entity and starts a coroutine that moves it to its waypoint
+
 
 
 

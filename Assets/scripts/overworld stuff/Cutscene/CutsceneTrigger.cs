60c3f243eeb8e5055d8c02ea9cb3643580be41bd
Graves <theredhead242@gmail.com>
2020-09-26 05:33:10 +0000

    cutscene save data
    Former-commit-id: 80d538350e10f839dcb2a73e8ec2eeb8a46cf091

--- Assets/scripts/overworld stuff/Cutscene/CutsceneTrigger.cs
+++ Assets/scripts/overworld stuff/Cutscene/CutsceneTrigger.cs
@@ -2,9 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+    so this also needs to check if the cutscene with the given id/name has been already triggered
+    we look into the playerprogress data and just send it this cutscenes id/name and then check if it's been triggered
+
+    probably dont want to have to manually create a variable for all these cutscenes though, so they can be saved 
+    seperately in a dynamic list 
+    save manager will hold a collection of all the cutscenes that have been triggered, which starts empty 
+    as cutscenes trigger they get added to the list that way not every cutscene needs a manual save
+
+*/
+
+
 public class CutsceneTrigger : MonoBehaviour
 {
+
+    //check if we should even load our collider, if this cutscene has been run before dont load it
+
+
+
     public Cutscene cutscene;
+
+    private void Start()
+    {
+
+
+        StartCoroutine(LateLateUpdate());
+    }
+
+    //runs after the second frame update
+    IEnumerator LateLateUpdate()
+    {
+        yield return new WaitForEndOfFrame();
+        yield return new WaitForEndOfFrame();
+
+        if (SaveManager.checkIfCutsceneRan(cutscene.cutsceneID))
+        {
+            //turn off the collider
+
+            GetComponent<Collider>().enabled = false;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -15,6 +53,9 @@ public class CutsceneTrigger : MonoBehaviour
             {
                 CutsceneManager.current.LoadCutscene(cutscene);
                 CutsceneManager.current.StartCutscene();
+
+                //disable the collider so we cant possibly 
+                GetComponent<Collider>().enabled = false;
             }
         }
     }

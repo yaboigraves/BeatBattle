acae3a762ad2e68a7076b36e9e1074dfd7aa5075
Graves <theredhead242@gmail.com>
2020-10-02 06:03:11 +0000

    refactor to battle time, waits still need to be implemented fully

--- Assets/scripts/battle stuff/BattleTrackManager.cs
+++ Assets/scripts/battle stuff/BattleTrackManager.cs
@@ -15,20 +15,14 @@ public class BattleTrackManager : MonoBehaviour
     public int battleBarsPerTurn = 4;
     public int battleTurn;
     public float currentBpm;
-
     public Track[] playerTracks;
     public Track[] testPlayerTracks;
     public Track[] testEnemyTracks;
-
     //if this is enabled play a sound when the metronome ticks
     public bool metronomeAudio;
-
     public int totalBeats;
-
     public int currentBar;
-
     public Track playerSelectedTrack;
-
     //used by the track time manager to hopefully setup bar waits between tracks coming out
     public float countInBeats = 4;
 
@@ -41,7 +35,6 @@ public class BattleTrackManager : MonoBehaviour
         if (TrackManager.current == null)
         {
             //initialize the indicator positions 
-
             testPlayerTracks[0].kickBeats.indicatorPositions = Array.ConvertAll(testPlayerTracks[0].kickBeats.indicatorData.Split(' '), float.Parse);
             testPlayerTracks[0].snareBeats.indicatorPositions = Array.ConvertAll(testPlayerTracks[0].snareBeats.indicatorData.Split(' '), float.Parse);
 
@@ -49,23 +42,13 @@ public class BattleTrackManager : MonoBehaviour
             playerSelectedTrack = testPlayerTracks[0];
             currentTrack = playerSelectedTrack;
             audioClip = testPlayerTracks[0].trackClip;
-
-
         }
         else
         {
             playerTracks = GameManager.current.player.GetComponent<PlayerInventory>().battleEquippedTracks;
-
             currentTrack = playerTracks[0];
-
             playerSelectedTrack = playerTracks[0];
-
-
-
             audioClip = currentTrack.trackClip;
-
-            //TODO: depending on if the player or the enemy goes first we grab different tracks
-            //
         }
 
         print("audiosource" + audioSource.name);
@@ -78,8 +61,6 @@ public class BattleTrackManager : MonoBehaviour
 
         //uniform speed for timescale change
         beatDeltaTime = 1;
-
-
     }
 
 
@@ -95,17 +76,8 @@ public class BattleTrackManager : MonoBehaviour
         //audioSource.Play();
         BattleManager.current.battleStarted = true;
 
-
-
-
         //so from right here, going to start weaving in the track time manager
-
-
-
         TrackTimeManager.current.startTrackTimer();
-
-
-        StartCoroutine(beatTick());
     }
 
     public void StopBattle()
@@ -116,31 +88,10 @@ public class BattleTrackManager : MonoBehaviour
 
     public void StartCountIn()
     {
-        //StartCoroutine(battleCountIn());
-        //StartBattle();
-
         TrackTimeManager.current.beatWait(4);
-
-
         //need to go to the tracktime manager and have it wait 4 beats, then reset all the timing data and actually start the song
     }
 
-    IEnumerator battleCountIn()
-    {
-        // shitty 1 2 3 4
-        BattleUIManager.current.UpdateMetronome(beat, true);
-        yield return new WaitForSeconds(beatDeltaTime);
-        beat++;
-        BattleUIManager.current.UpdateMetronome(beat, true);
-        yield return new WaitForSeconds(beatDeltaTime);
-        beat++;
-        BattleUIManager.current.UpdateMetronome(beat, true);
-        yield return new WaitForSeconds(beatDeltaTime);
-        beat++;
-        BattleUIManager.current.UpdateMetronome(beat, true);
-        yield return new WaitForSeconds(beatDeltaTime);
-        StartBattle();
-    }
 
     public int nextTurnStart;
     public void switchBattleTrack(Track newTrack, bool doWait)
@@ -149,7 +100,6 @@ public class BattleTrackManager : MonoBehaviour
         //1. we turn off the current track audio
         audioSource.Stop();
         //2.we load in the new track as the current track
-
         currentTrack = newTrack;
         //3.replace the audiosource's clip
 
@@ -161,7 +111,6 @@ public class BattleTrackManager : MonoBehaviour
         //6.play the new track AFTER 4 BARS OF WAITING if a wait is requested
 
         nextTurnStart = totalBeats + 5;
-
 
         TrackTimeManager.current.SetSongData(currentTrack);
         TrackTimeManager.current.stopTrackTimer();
@@ -175,60 +124,7 @@ public class BattleTrackManager : MonoBehaviour
         }
     }
 
-    IEnumerator barWait()
-    {
-        yield return new WaitUntil(() => totalBeats == nextTurnStart);
-        print("bar waiting playing");
-        audioSource.Play();
-        TrackTimeManager.current.startTrackTimer();
-    }
-
-
     //so we need a centralized place for the time passage to be managed from
-    IEnumerator beatTick()
-    {
-        //if we're paused dont do any waiting
-        while (paused)
-        {
-            yield return null;
-        }
-
-        //check where the vibes at and deal damage if needed to the player
-
-        BattleManager.current.VibeUpdate();
-
-        //update the beat
-        beat++;
-
-        //tell the battle manager to update the beat info (for now it updates every beat)
-        BattleManager.current.UpdateGearPipeline();
-
-        if (beat > 3)
-        {
-
-            beat = 0;
-            battleTurn++;
-
-            if (battleTurn >= battleBarsPerTurn)
-            {
-                BattleManager.current.changeTurn();
-                battleTurn = 0;
-            }
-            BattleCameraController.current.CameraSwitchup();
-        }
-
-        // BattleUIManager.current.UpdateMetronome(beat, false);
-
-        totalBeats++;
-        currentBar = (int)Mathf.Floor(totalBeats / 4.0f);
-
-        //tell the camera to switchup maybe 
-
-
-
-        yield return new WaitForSeconds(beatDeltaTime);
-        StartCoroutine(beatTick());
-    }
 
     public void setPlayerSelectedTrack(Track newTrack)
     {

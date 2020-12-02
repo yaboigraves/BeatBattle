0ebd7e01d5f4d06c40d591cbf8334c2e7e06b575
Graves <theredhead242@gmail.com>
2020-12-01 22:24:02 -0800

    progrids implimented, fall catcher, minor level prototypes
    Former-commit-id: 799b7842693ce71cc0e6410c31f1e8fa6ceb65a0

---
+++ Assets/scripts/overworld stuff/utility/LoadAreaTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadAreaTrigger : MonoBehaviour
+{
+
+    public string sceneToLoad;
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManage.current.loadLevel(sceneToLoad, GameManager.current.player.transform.position);
+        }
+    }
+
+}

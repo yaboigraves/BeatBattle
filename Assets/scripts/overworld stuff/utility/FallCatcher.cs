799b7842693ce71cc0e6410c31f1e8fa6ceb65a0
Graves <theredhead242@gmail.com>
2020-12-01 22:24:02 -0800

    progrids implimented, fall catcher, minor level prototypes

---
+++ Assets/scripts/overworld stuff/utility/FallCatcher.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallCatcher : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Transform playerSpawnPos;
+    private void Start()
+    {
+        playerSpawnPos = GameObject.FindGameObjectWithTag("playerSpawn").transform;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //just chuck the player to their spawn for now, probably want to penalize the player somehow 
+            //also make a ui fade
+
+            other.gameObject.transform.position = playerSpawnPos.position;
+        }
+    }
+
+}

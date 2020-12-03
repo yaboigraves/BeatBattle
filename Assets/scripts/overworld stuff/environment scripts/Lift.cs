9a950b1399871594abc3a06cb0c9ed7bda955002
Graves <theredhead242@gmail.com>
2020-12-02 23:21:51 -0800

    basic activeateable objects

---
+++ Assets/scripts/overworld stuff/environment scripts/Lift.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lift : MonoBehaviour, IActivateable
+{
+    // Start is called before the first frame update
+
+    public float speed = 1f;
+    public float delta = 3f;
+
+    private void Start()
+    {
+        enabled = false;
+    }
+
+    public void Activate()
+    {
+        enabled = !enabled;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float y = Mathf.PingPong(speed * Time.time, delta);
+        Vector3 pos = new Vector3(transform.position.x, y, transform.position.z);
+        transform.position = pos;
+    }
+}

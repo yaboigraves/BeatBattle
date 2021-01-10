7d99e7a54afb2a5d4911249d12639ca19ca0331e
Graves <theredhead242@gmail.com>
2021-01-09 17:57:52 -0800

    basic turns removed full on guitar hero

--- Assets/scripts/battle stuff/battle ui/IndicatorManager.cs
+++ Assets/scripts/battle stuff/battle ui/IndicatorManager.cs
@@ -39,14 +39,14 @@ public class IndicatorManager : MonoBehaviour
 
         //TODO: this dont really need to be dynamically instantiated they can just be part of the container prefab
 
-        for (int i = 0; i <= 16; i++)
+        for (int i = 0; i <= 64; i++)
         {
             GameObject _bar = Instantiate(bar, Vector3.up * (100 + i), Quaternion.identity, indicContainer.transform.GetChild(1));
         }
 
         for (int i = 0; i < track.kickBeats.Count; i++)
         {
-            Vector3 kickPos = new Vector3(-1, 0 + 100 + ((float)track.kickBeats[i]), 0);
+            Vector3 kickPos = new Vector3(-1, 4.1f + 100 + ((float)track.kickBeats[i]), 0);
             //each unit is 1 bar 
             //therefore we need to start the next batck of indicators at wherever the loop ends
             //probablyh easiest for now just to bake the length of the loop into the track object 
@@ -59,7 +59,7 @@ public class IndicatorManager : MonoBehaviour
 
         for (int i = 0; i < track.snareBeats.Count; i++)
         {
-            Vector3 kickPos = new Vector3(1, 0 + 100 + ((float)track.snareBeats[i]), 0);
+            Vector3 kickPos = new Vector3(1, 4.1f + 100 + ((float)track.snareBeats[i]), 0);
             GameObject indic = Instantiate(indicator, kickPos, Quaternion.identity, indicContainer.transform.GetChild(0));
 
 

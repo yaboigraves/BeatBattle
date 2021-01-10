db226b5f824c357467f1123698593a9033474e4c
Graves <theredhead242@gmail.com>
2021-01-09 20:39:11 -0800

    basic battle console
    Former-commit-id: 901feeee5143ee9d7fbf224f78b40a6e25cdba25

---
+++ Assets/scripts/battle stuff/battle ui/debug console/BattleDebugConsole.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/*
+    general manager for manipulating settings for the battle
+
+*/
+
+
+public class BattleDebugConsole : MonoBehaviour
+{
+    public GameObject debugPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ActivateConsole()
+    {
+        debugPanel.SetActive(!debugPanel.activeSelf);
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
+    }
+}

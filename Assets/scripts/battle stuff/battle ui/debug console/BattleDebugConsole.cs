412706e77954aa9df6f08e43bb02e81dffe05cbf
Graves <theredhead242@gmail.com>
2021-01-10 17:51:35 -0800

    track selection

--- Assets/scripts/battle stuff/battle ui/debug console/BattleDebugConsole.cs
+++ Assets/scripts/battle stuff/battle ui/debug console/BattleDebugConsole.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 /*
     general manager for manipulating settings for the battle
@@ -13,11 +14,12 @@ using UnityEngine.SceneManagement;
 public class BattleDebugConsole : MonoBehaviour
 {
     public GameObject debugPanel;
+    public TMP_Dropdown trackSelector;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        InitTrackSelector();
     }
 
     // Update is called once per frame
@@ -48,13 +50,26 @@ public class BattleDebugConsole : MonoBehaviour
         IndicatorManager.current.ClearIndicators();
         IndicatorManager.current.ClearBars();
 
-        BattleManager.current.StopBattle();
-
-        BattleManager.current.setupBattle();
-
-
         //after all this shit is done tell the battlemanager to reload the currently selected track and set the battle to not started
+        BattleManager.current.StopBattle();
+        BattleManager.current.setupBattle();
+    }
 
+    public void InitTrackSelector()
+    {
+        //go through all the tracks in the battletrack manager test tracks and create a dropdown option for each of them
+        trackSelector.ClearOptions();
 
+        foreach (Track t in BattleTrackManager.current.testPlayerTracks)
+        {
+            trackSelector.options.Add(new TMP_Dropdown.OptionData(t.name));
+        }
+    }
+
+    public void SelectTrack(int selection)
+    {
+        //set the current track to the selected option and then reset the scene
+        BattleTrackManager.current.playerSelectedTrack = BattleTrackManager.current.testPlayerTracks[selection];
+        ReloadScene();
     }
 }

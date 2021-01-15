6e3d7cfb17a05d8b3e5a7cdae7dc992f2134b4e2
Graves <theredhead242@gmail.com>
2021-01-14 22:37:57 -0800

    cutscene phase handling

--- Assets/scripts/overworld stuff/Cutscene/NewCutsceneManager.cs
+++ Assets/scripts/overworld stuff/Cutscene/NewCutsceneManager.cs
@@ -12,6 +12,11 @@ public class NewCutsceneManager : MonoBehaviour
     public static NewCutsceneManager current;
     public DialogueRunner dialogueRunner;
 
+    //assuming that if you have a cutscene in a dialogue there's a pause somewhere
+    public bool pauseScheduled = true;
+
+
+
     public PlayableDirector director;
     private void Awake()
     {
@@ -22,17 +27,32 @@ public class NewCutsceneManager : MonoBehaviour
         dialogueRunner.AddCommandHandler("startCutscene", startCutscene);
         dialogueRunner.AddCommandHandler("cutsceneCheckpoint", cutsceneCheckpoint);
 
+
+
     }
 
     public void startCutscene(string[] parameters)
     {
+        pauseScheduled = true;
         director.Play();
+
     }
 
     //this is emitted from the cutscene when we need to pause stuff and wait for yarn to progress forward
     public void PauseCutscene()
     {
-        director.playableGraph.GetRootPlayable(0).SetSpeed(0d);
+        //so we're only gonna pause if their is still a pause scheduled
+
+        if (pauseScheduled)
+        {
+            director.playableGraph.GetRootPlayable(0).SetSpeed(0d);
+            pauseScheduled = false;
+        }
+        else
+        {
+            //if theres no pause scheduled then we re assume theres gonna be a pause
+            pauseScheduled = true;
+        }
     }
 
     //this is called from yarn and when that happens we can resume the timeline
@@ -45,7 +65,21 @@ public class NewCutsceneManager : MonoBehaviour
     */
     public void cutsceneCheckpoint(string[] parameters)
     {
-        director.playableGraph.GetRootPlayable(0).SetSpeed(1d);
+        //so if their is a pause scheduled and we get here then we're just gonna say no more pause
+        if (pauseScheduled)
+        {
+            pauseScheduled = false;
+        }
+        else
+        {
+            //if theres no pause scheduled and we get to a checkpoint i think we assume theirs gonna be a pause
+            pauseScheduled = true;
+            director.playableGraph.GetRootPlayable(0).SetSpeed(1d);
+
+        }
+
+        //if theres no pause scheduled and we get here
+
     }
 
 

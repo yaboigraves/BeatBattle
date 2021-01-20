7199a83407bb1423d0c871dd5fc7dd43bb21311c
Graves <theredhead242@gmail.com>
2021-01-19 16:04:14 -0800

    working on cutscene trigger rewrite

---
+++ Assets/scripts/overworld stuff/Cutscene/NewCutscene.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+
+[CreateAssetMenu(fileName = "New Track", menuName = "Cutscene/Cutscene")]
+
+public class NewCutscene : ScriptableObject
+{
+    public TimelineAsset cutscene;
+
+    // Start is called before the first frame update
+
+
+
+}

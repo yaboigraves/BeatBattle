96fac57e98674f2d7f9755f3b2db2039673fc85f
Graves <theredhead242@gmail.com>
2021-01-27 15:24:19 -0800

    transition and track clip + midi parsing, just needs bpm scanning then done
    Former-commit-id: e242af8f1c7edf3dead66f0570be7325a1fe766e

--- Assets/scripts/audio/Track.cs
+++ Assets/scripts/audio/Track.cs
@@ -42,49 +42,60 @@ public class Track : GameItem
             FOR THE MOST PART THE TRACKLOADER DOES ALL THE HEAVY LIFTING TO KEEP THIS FILE FROM IMPORTING HELLA LIBS
         */
 
-
-
-
-
         if (trackStats.mixType == TrackStats.MixType.QuickMix)
         {
-
             //TODO: check if this works in builds dynamically
-            AudioClip[] trackClips = TrackLoader.loadAudioClips(trackName);
+            AudioClip[] trackClips = TrackLoader.loadAudioClips(trackName, false);
+            AudioClip[] transitionClips = TrackLoader.loadAudioClips(trackName, true);
+
+            Debug.Log("transiction clip length" + transitionClips.Length.ToString());
 
             //so to get all the midi data the trackloader is also going to need a function to calculate that all
-
-
-
-            Dictionary<string, List<System.Double>>[] midiData = TrackLoader.loadMidis(trackName);
-
-
+            Dictionary<string, List<System.Double>>[] tracksMidiData = TrackLoader.loadMidis(trackName, false);
+            Dictionary<string, List<System.Double>>[] transitionsMidiData = TrackLoader.loadMidis(trackName, true);
 
             //go through all the trackclips and create a track data for them
             TrackData[] tData = new TrackData[trackClips.Length];
+            TransitionData[] transData = new TransitionData[transitionClips.Length];
+
 
             for (int i = 0; i < trackClips.Length; i++)
             {
                 tData[i].trackClip = trackClips[i];
-                //TODO: load the midi too lol
-
             }
 
-            for (int i = 0; i < midiData.Length; i++)
+            for (int i = 0; i < transitionClips.Length; i++)
             {
-                tData[i].kickBeats = midiData[i]["kick"];
-                tData[i].hatBeats = midiData[i]["hat"];
-                tData[i].snareBeats = midiData[i]["snare"];
-                tData[i].percBeats = midiData[i]["perc"];
+                transData[i].transitionClip = transitionClips[i];
+            }
+
+            for (int i = 0; i < tracksMidiData.Length; i++)
+            {
+                tData[i].kickBeats = tracksMidiData[i]["kick"];
+                tData[i].hatBeats = tracksMidiData[i]["hat"];
+                tData[i].snareBeats = tracksMidiData[i]["snare"];
+                tData[i].percBeats = tracksMidiData[i]["perc"];
+            }
+
+
+            for (int i = 0; i < transitionsMidiData.Length; i++)
+            {
+                if (i >= transData.Length)
+                {
+                    Debug.LogError("NOT ENOUGH DATA SPOTS FOR NEW MIDI");
+                    break;
+                }
+                transData[i].kickBeats = transitionsMidiData[i]["kick"];
+                transData[i].hatBeats = transitionsMidiData[i]["hat"];
+                transData[i].snareBeats = transitionsMidiData[i]["snare"];
+                transData[i].percBeats = transitionsMidiData[i]["perc"];
             }
 
             tracks = tData;
+            trackTransitions = transData;
 
             //after we do all this we need to load the transition data as well for the track, basically the same process 
             //but the filepath differs
-
-
-
         }
         else if (trackStats.mixType == TrackStats.MixType.LongMix)
         {
